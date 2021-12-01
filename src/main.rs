171ef2940361f47ba091a17069fdd66807e6fe75
zylkowski_a <arkadiuszzylkowski@gmail.com>
2021-12-01 17:19:18 +0100

    Added bgamma relation

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,11 @@ extern crate glium;
 use glium::glutin;
 // use egui::*;
 
-const WINDOW_SIZE: (u32, u32) = (1530, 520);
-const CHART_SIZE: (usize, usize) = (650, 500);
+const WINDOW_SIZE: (u32, u32) = (1330, 520);
+const CHART_SIZE: (usize, usize) = (550, 500);
 
 struct ChartConfig {
-    a: usize,
+    a: f64,
     z_big: usize,
 
     z: f64,
@@ -55,31 +55,64 @@ fn create_display(event_loop: &glutin::event_loop::EventLoop<()>) -> glium::Disp
 fn draw_bethe(chart_info: &ChartConfig, painter: &mut egui_glium::Painter, texture_id: TextureId) {
     let mut png_data: Vec<u8> = vec![0; CHART_SIZE.0 * CHART_SIZE.1 * 3];
     {
-        let root_area =
+        let root =
             BitMapBackend::with_buffer(&mut png_data, (CHART_SIZE.0 as u32, CHART_SIZE.1 as u32))
                 .into_drawing_area();
-        root_area.fill(&RGBColor(40, 40, 40)).unwrap();
-        let log_range = LogCoord::from(LogRange(chart_info.bethe_min_x..chart_info.bethe_max_x));
+        root.fill(&RGBColor(40, 40, 40)).unwrap();
+        let (root_area, lower_axis_area) = root.split_vertically((87).percent());
+
+        let log_range_bg = LogCoord::from(LogRange(chart_info.bethe_min_x..chart_info.bethe_max_x));
+
+        let lower_bound = |value: f64| -> f64 {
+            if value.is_nan() {
+                0.
+            } else {
+                value
+            }
+        }((e_kin(chart_info.bethe_min_x, 200. * 0.511) * 0.001));
+
+        let log_range_muon = LogCoord::from(LogRange(
+            lower_bound..(e_kin(chart_info.bethe_max_x, 200. * 0.511) * 0.001),
+        ));
+
+        let mut lower_ax_ctx = ChartBuilder::on(&lower_axis_area)
+            .margin_left(20)
+            .margin_right(20)
+            .margin_bottom(10)
+            .set_label_area_size(LabelAreaPosition::Left, 60)
+            .set_label_area_size(LabelAreaPosition::Bottom, 45)
+            .build_cartesian_2d(log_range_muon, 0. ..10.)
+            .unwrap();
+        lower_ax_ctx
+            .configure_mesh()
+            .disable_y_axis()
+            .disable_y_mesh()
+            .bold_line_style(&WHITE)
+            .axis_style(&WHITE)
+            .x_desc("Muon kinetic energy [GeV]")
+            .label_style(("sans-serif", 15, &WHITE))
+            .draw()
+            .unwrap();
 
         let mut ctx = ChartBuilder::on(&root_area)
             .set_label_area_size(LabelAreaPosition::Left, 60)
             .set_label_area_size(LabelAreaPosition::Bottom, 40)
             .margin_left(20)
             .margin_right(20)
-            .margin_bottom(10)
+            // .margin_bottom(10)
             .caption("Bethe-Bloch", ("sans-serif", 40, &WHITE))
-            .build_cartesian_2d(log_range, chart_info.bethe_min_y..chart_info.bethe_max_y)
+            .build_cartesian_2d(log_range_bg, chart_info.bethe_min_y..chart_info.bethe_max_y)
             .unwrap();
 
         //
 
-        let resolution = 200.;
-        let points = (chart_info.bethe_max_x - chart_info.bethe_min_x) * resolution;
+        let points = 2000000;
+        let step = (chart_info.bethe_max_x - chart_info.bethe_min_x) / (points as f64);
         let range = (0..=(points as i128));
 
         ctx.draw_series(LineSeries::new(
             (range).map(|x| {
-                let real_x = chart_info.bethe_min_x + (x as f64) / resolution;
+                let real_x = (x as f64) * step;
                 let value = stopping_power(
                     chart_info.a,
                     chart_info.z_big,
@@ -90,7 +123,11 @@ fn draw_bethe(chart_info: &ChartConfig, painter: &mut egui_glium::Painter, textu
                 );
                 (real_x, value)
             }),
-            &RED,
+            ShapeStyle {
+                color: RED.mix(1.),
+                filled: true,
+                stroke_width: 2,
+            },
         ))
         .unwrap();
 
@@ -133,8 +170,8 @@ fn draw_energy(chart_info: &ChartConfig, painter: &mut egui_glium::Painter, text
             )
             .unwrap();
 
-        let resolution = 200.;
-        let points = (chart_info.energy_max_x - chart_info.energy_min_x) * resolution;
+        let points = 2000000;
+        let step = (chart_info.energy_max_x - chart_info.energy_min_x) / (points as f64);
         let range = (0..=(points as i128));
 
         // Electron, Muon, Pi, D, alpha
@@ -150,15 +187,15 @@ fn draw_energy(chart_info: &ChartConfig, painter: &mut egui_glium::Painter, text
         for particle in particles {
             ctx.draw_series(LineSeries::new(
                 (range.clone()).map(|x| {
-                    let real_x = chart_info.energy_min_x + (x as f64) / resolution;
+                    let real_x = (x as f64) * step;
                     let value = e_kin(real_x, particle.0);
-                    // println!(
-                    //     "real_x: {} ekin: {}, particle_mas {}",
-                    //     real_x, value, particle.0
-                    // );
                     (real_x, value)
                 }),
-                particle.1,
+                ShapeStyle {
+                    color: particle.1.mix(1.),
+                    filled: true,
+                    stroke_width: 2,
+                },
             ))
             .unwrap()
             .label(particle.2)
@@ -168,7 +205,7 @@ fn draw_energy(chart_info: &ChartConfig, painter: &mut egui_glium::Painter, text
         ctx.configure_mesh()
             .bold_line_style(&WHITE)
             .axis_style(&WHITE)
-            .y_desc("E [MeV]")
+            .y_desc("E_k [MeV]")
             .x_desc("βγ")
             .label_style(("sans-serif", 15, &WHITE))
             .draw()
@@ -197,37 +234,28 @@ fn e_kin(beta_gamma: f64, mass: f64) -> f64 {
     (mass * (gamma - 1.))
 }
 
-fn stopping_power(a: usize, z_big: usize, z: f64, beta_gamma: f64, t_max: f64, delta: f64) -> f64 {
+fn stopping_power(a: f64, z_big: usize, z: f64, beta_gamma: f64, t_max: f64, delta: f64) -> f64 {
     const K: f64 = 0.3072;
-    const M_e: f64 = 0.511;
-    // const C: f64 = 1.;
-    const C: f64 = 300000000.;
 
-    let i = 10. * z_big as f64;
+    let i = 1e-5 * z_big as f64;
     let beta_2 = beta_gamma.powi(2) / (beta_gamma.powi(2) + 1.);
 
-    K * ((z_big as f64) / (a as f64 * beta_2)) * ((w_m(beta_2) / i).ln() - beta_2)
+    K * ((z_big as f64) / (a * beta_2)) * ((w_m(beta_gamma) / i).ln() - beta_2)
 }
 
-fn w_m(beta_2: f64) -> f64 {
-    const C: f64 = 300000000.;
-    const C: f64 = 1.;
+fn w_m(beta_gamma: f64) -> f64 {
     const M_e: f64 = 0.511;
-    println!("W_m = {}", (2. * M_e * C.powi(2) * beta_2) / (1. - beta_2));
-    (2. * M_e * C.powi(2) * beta_2) / (1. - beta_2)
+    (2. * M_e * beta_gamma.powi(2))
 }
 
 fn main() {
-    // print!("E_kin prot 1 betagamma = {}", e_kin(1., 1836. * 0.511));
-
-    // return;
     let event_loop = glutin::event_loop::EventLoop::with_user_event();
     let display = create_display(&event_loop);
 
     let mut egui = egui_glium::EguiGlium::new(&display);
 
     let mut chart_info = ChartConfig {
-        a: 1,
+        a: 1.,
         z_big: 1,
         z: 1.,
         t_max: 10.,
@@ -237,7 +265,7 @@ fn main() {
         bethe_min_x: 0.,
 
         bethe_min_y: 0.,
-        bethe_max_y: 200.,
+        bethe_max_y: 20.,
 
         energy_min_x: 0.,
         energy_max_x: 5.,
@@ -268,7 +296,11 @@ fn main() {
                     ui.horizontal(|ui| {
                         ui.label("A: ");
                         recalculate_chart = ui
-                            .add(egui::DragValue::new(&mut chart_info.a).clamp_range(1..=300))
+                            .add(
+                                egui::DragValue::new(&mut chart_info.a)
+                                    .clamp_range(1..=300)
+                                    .suffix(" u"),
+                            )
                             .changed()
                             || recalculate_chart;
                         ui.label("Z: ");
